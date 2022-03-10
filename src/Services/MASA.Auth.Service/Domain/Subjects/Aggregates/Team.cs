0473dff5d37fba7887fe5c560930aec773f93508
wuweilai <wuweilai@masastack.com>
2022-03-10 18:13:47 +0800

    最新代码提交

--- src/Services/MASA.Auth.Service/Domain/Subjects/Aggregates/Team.cs
+++ src/Services/MASA.Auth.Service/Domain/Subjects/Aggregates/Team.cs
@@ -4,9 +4,13 @@ public class Team : AuditAggregateRoot<Guid, Guid>
 {
     public string Name { get; private set; }
 
-    public string? Avatar { get; private set; }
+    public string Avatar { get; private set; }
 
-    public string? Describe { get; private set; }
+    public string AvatarName { get; set; }
+
+    public string Describe { get; private set; }
+
+    public TeamTypes TeamType { get; private set; }
 
     public List<TeamStaff> Staffs { get; private set; } = new();
 
@@ -14,11 +18,13 @@ public class Team : AuditAggregateRoot<Guid, Guid>
 
     public List<TeamRole> Roles { get; private set; } = new();
 
-    public Team(string name, string? avatar, string? describe)
+    public Team(string name, string avatar,string avatarName, string describe, TeamTypes teamType)
     {
         Name = name;
         Avatar = avatar;
+        AvatarName = avatarName;
         Describe = describe;
+        TeamType = teamType;
     }
 }
 
