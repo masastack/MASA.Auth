6e0b956ee7f97e2038949a8bdd83ff359096b7db
wuweilai <wuweilai@masastack.com>
2022-03-14 14:33:00 +0800

    configure BaseAddress

---
+++ src/Sdk/MASA.Auth.Sdk/Options.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masa.Auth.Sdk
+{
+    public class Options
+    {
+        public string AuthServiceBaseAdress { get; set; }
+
+        public Options(string authServiceBaseAdress)
+        {
+            AuthServiceBaseAdress = authServiceBaseAdress;
+        }
+    }
+}
--- src/Sdk/MASA.Auth.Sdk/ServiceCollectionExtensions.cs
+++ src/Sdk/MASA.Auth.Sdk/ServiceCollectionExtensions.cs
@@ -4,8 +4,13 @@ namespace Masa.Auth.Sdk;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddAuthSdkServer(this IServiceCollection services)
+    public static IServiceCollection AddAuthSdkServer(this IServiceCollection services,Action<Options>? configure = null)
     {
+        var options = new Options("http://localhost:8080/");
+        //Todo default option
+
+        configure?.Invoke(options);
+        services.AddSingleton(options);
         services.AddScoped<AuthClient>();
 
         return services;
