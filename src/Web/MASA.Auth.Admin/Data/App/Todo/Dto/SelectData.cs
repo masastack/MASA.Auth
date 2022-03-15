e82499cacfdb7e2ffb43ee76427fe4cb25592fd8
Mayue <mayue@lonsid.cn>
2022-03-15 13:26:11 +0800

    feat:remove repeat files

--- src/Web/MASA.Auth.Admin/Data/App/Todo/Dto/SelectData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Masa.Auth.Admin.Data.App.Todo.Dto
-{
-    public class SelectData
-    {
-        public string Label { get; set; } = default!;
-
-        public string Value { get; set; } = default!;
-    }
-}
