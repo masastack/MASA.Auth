647f920f522e52586da77af3c7778f938e83e0de
wuweilai <wuweilai@masastack.com>
2022-03-23 10:39:40 +0800

    user page

--- src/Contracts/Masa.Auth.Contracts.Admin/Subjects/UserDetailDto.cs
+++ src/Contracts/Masa.Auth.Contracts.Admin/Subjects/UserDetailDto.cs
@@ -4,11 +4,23 @@ public class UserDetailDto : UserDto
 { 
     public AddressValueDto Address { get; set; }
 
-    public static UserDetailDto Default => new UserDetailDto(Guid.Empty, "", "", "", "", "", "", default, "", "", DateTime.Now, new());
+    public List<string> ThirdPartyIdpAvatars { get; set; }
 
-    public UserDetailDto(Guid userId, string name, string displayName, string avatar, string iDCard, string account, string companyName, bool enabled, string phoneNumber, string email, DateTime creationTime, AddressValueDto address) : base(userId, name, displayName, avatar, iDCard, account, companyName, enabled, phoneNumber, email, creationTime)
+    public string Creator { get; set; }
+
+    public string Modifier { get; set; }
+
+    public DateTime? ModificationTime { get; set; }
+
+    public static UserDetailDto Default => new UserDetailDto(Guid.Empty, "", "", "", "", "", "", default, "", "", DateTime.Now, new(), new(), "","",default);
+
+    public UserDetailDto(Guid userId, string name, string displayName, string avatar, string iDCard, string account, string companyName, bool enabled, string phoneNumber, string email, DateTime creationTime, AddressValueDto address, List<string> thirdPartyIdpAvatars, string creator, string modifier, DateTime? modificationTime) : base(userId, name, displayName, avatar, iDCard, account, companyName, enabled, phoneNumber, email, creationTime)
     {
         Address = address;
+        ThirdPartyIdpAvatars = thirdPartyIdpAvatars;
+        Creator = creator;
+        Modifier = modifier;
+        ModificationTime = modificationTime;
     }
 }
 
