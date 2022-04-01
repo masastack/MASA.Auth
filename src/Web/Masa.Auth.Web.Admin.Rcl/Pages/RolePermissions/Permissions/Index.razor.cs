0816fdddd4633a187be04fa53341c30c3fee5994
Mayue <mayue@lonsid.cn>
2022-04-01 18:24:18 +0800

    feat:init permission page(incomplete)

---
+++ src/Web/Masa.Auth.Web.Admin.Rcl/Pages/RolePermissions/Permissions/Index.razor.cs
@@ -0,0 +1,35 @@
+﻿namespace Masa.Auth.Web.Admin.Rcl.Pages.RolePermissions.Permissions;
+
+public partial class Index
+{
+    private List<string> _values = new List<string>
+    {
+        "foo", "bar"
+    };
+    private List<string> _items = new List<string>
+    {
+        "foo", "bar", "fizz", "buzz"
+    };
+
+    private string value3 = "";
+
+    public class Item
+    {
+        public string Label { get; set; }
+        public string Value { get; set; }
+
+        public Item(string label, string value)
+        {
+            Label = label;
+            Value = value;
+        }
+    }
+
+    List<Item> items = new()
+    {
+        new Item("Foo", "1"),
+        new Item("Bar", "2"),
+        new Item("Fizz", "3"),
+        new Item("Buzz", "4"),
+    };
+}
