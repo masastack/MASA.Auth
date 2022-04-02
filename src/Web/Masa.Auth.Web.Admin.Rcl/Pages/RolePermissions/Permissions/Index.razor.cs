792ca16e2d64dd6818b6d0267a296e66adcffb47
Mayue <mayue@lonsid.cn>
2022-04-02 22:03:54 +0800

    feat:sheet dialog

--- src/Web/Masa.Auth.Web.Admin.Rcl/Pages/RolePermissions/Permissions/Index.razor.cs
+++ src/Web/Masa.Auth.Web.Admin.Rcl/Pages/RolePermissions/Permissions/Index.razor.cs
@@ -2,6 +2,11 @@
 
 public partial class Index
 {
+    StringNumber _tab = 0;
+    List<PermissionDto> _menuPermissions = new();
+    List<PermissionDto> _apiPermissions = new();
+    List<Guid> _menuActive = new List<Guid>();
+    List<Guid> _apiActive = new List<Guid>();
     private List<string> _values = new List<string>
     {
         "foo", "bar"
@@ -10,6 +15,7 @@ public partial class Index
     {
         "foo", "bar", "fizz", "buzz"
     };
+    bool _isShow = true;
 
     private string value3 = "";
 
@@ -32,4 +38,43 @@ public partial class Index
         new Item("Fizz", "3"),
         new Item("Buzz", "4"),
     };
+
+    protected override Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            _menuPermissions = new List<PermissionDto>()
+            {
+                new PermissionDto() { Name = "菜单1", Code = "1",Id=Guid.NewGuid(),Children = new List<PermissionDto>
+                    {
+                        new PermissionDto() { Name ="11111",Id=Guid.NewGuid()},
+                        new PermissionDto() { Name ="222233",Id=Guid.NewGuid()}
+                    }
+                },
+                new PermissionDto() { Name = "菜单2", Code = "1",Id=Guid.NewGuid(),Children = new List<PermissionDto>
+                    {
+                        new PermissionDto() { Name ="44444",Id=Guid.NewGuid()},
+                        new PermissionDto() { Name ="555555",Id=Guid.NewGuid()}
+                    }
+                }
+            };
+            _apiPermissions = new List<PermissionDto>()
+            {
+                new PermissionDto() { Name = "Api1", Code = "1",Id=Guid.NewGuid(),Children = new List<PermissionDto>
+                    {
+                        new PermissionDto() { Name ="11111",Id=Guid.NewGuid()},
+                        new PermissionDto() { Name ="222233",Id=Guid.NewGuid()}
+                    }
+                },
+                new PermissionDto() { Name = "Api2", Code = "1",Id=Guid.NewGuid(),Children = new List<PermissionDto>
+                    {
+                        new PermissionDto() { Name ="44444",Id=Guid.NewGuid()},
+                        new PermissionDto() { Name ="555555",Id=Guid.NewGuid()}
+                    }
+                }
+            };
+            StateHasChanged();
+        }
+        return base.OnAfterRenderAsync(firstRender);
+    }
 }
