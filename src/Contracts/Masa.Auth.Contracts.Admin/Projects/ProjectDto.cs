e74fc9185e18e0c74220378d0bf3e795ce9d3add
Mayue <mayue@lonsid.cn>
2022-04-12 15:02:40 +0800

    feat:empty

---
+++ src/Contracts/Masa.Auth.Contracts.Admin/Projects/ProjectDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Masa.Auth.Contracts.Admin.Projects;
+
+public class ProjectDto
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Identity { get; set; } = string.Empty;
+}
