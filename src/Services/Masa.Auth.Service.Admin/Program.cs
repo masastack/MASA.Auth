afbbf10a049069e8f33a4c0b39b6403b11db5b88
MayueCif <6133287+MayueCif@users.noreply.github.com>
2022-04-18 09:30:24 +0800

    Feat:add pm invoke and Isolation(uncompleted) (#18)
    * feat:add pcclient and Isolation(uncompleted)
    
    * feat:update pm invoke
    
    * feat:modify for pr
    
    Co-authored-by: Mayue <mayue@lonsid.cn>

--- src/Services/Masa.Auth.Service.Admin/Program.cs
+++ src/Services/Masa.Auth.Service.Admin/Program.cs
@@ -1,4 +1,4 @@
-﻿using Masa.Utils.Data.EntityFrameworkCore.Filters;
+﻿using Masa.Contrib.Isolation.MultiEnvironment;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,8 +28,14 @@ builder.Services.AddAuthentication(options =>
 //);
 
 builder.Services.AddMasaRedisCache(builder.Configuration.GetSection("RedisConfig"));
-
-builder.Services.AddScoped<ISaveChangesFilter, SoftDeleteSaveChangesFilter>();
+builder.Services.AddPmClient(callerOption =>
+{
+    callerOption.UseHttpClient(_builder =>
+    {
+        _builder.Name = builder.Configuration.GetValue<string>("PmClient:Name");
+        _builder.Configure = opt => opt.BaseAddress = new Uri(builder.Configuration.GetValue<string>("PmClient:Url"));
+    });
+});
 
 var app = builder.Services
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -73,10 +79,19 @@ var app = builder.Services
             eventBusBuilder.UseMiddleware(typeof(ValidatorMiddleware<>));
             eventBusBuilder.UseMiddleware(typeof(LogMiddleware<>));
         })
-        .UseUoW<AuthDbContext>(dbOptions => dbOptions.UseSqlServer().UseSoftDelete())
+        .UseIsolationUoW<AuthDbContext>(
+            isolationBuilder => isolationBuilder.UseMultiEnvironment("env"),
+            dbOptions => dbOptions.UseSqlServer().UseSoftDelete())
         .UseRepository<AuthDbContext>();
     })
     .AddServices(builder);
+
+//set Isolation
+app.Use(async (context, next) =>
+{
+    context.Items.Add("env", "development");
+    await next.Invoke();
+});
 
 app.MigrateDbContext<AuthDbContext>((context, services) =>
 {
