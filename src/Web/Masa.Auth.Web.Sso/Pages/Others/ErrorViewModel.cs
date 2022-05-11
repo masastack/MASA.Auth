d0fd2e78ea4cbe1f030419261c72ed4ab7a0a616
Mayue <mayue@lonsid.cn>
2022-05-11 13:13:57 +0800

    feat:update 500 page

---
+++ src/Web/Masa.Auth.Web.Sso/Pages/Others/ErrorViewModel.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Auth.Web.Sso.Pages.Others;
+
+public class ErrorViewModel
+{
+    public ErrorViewModel()
+    {
+    }
+
+    public ErrorViewModel(string error)
+    {
+        Error = new ErrorMessage { Error = error };
+    }
+
+    public ErrorMessage Error { get; set; }
+}
