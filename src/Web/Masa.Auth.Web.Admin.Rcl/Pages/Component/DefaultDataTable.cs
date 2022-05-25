e40fae8a9c51d954b83b225a76a5d26bb7436789
wuweilai <824255785@qq.com>
2022-05-25 17:34:34 +0800

    ui

---
+++ src/Web/Masa.Auth.Web.Admin.Rcl/Pages/Component/DefaultDataTable.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Auth.Web.Admin.Rcl.Pages.Component;
+
+public class DefaultDataTable<TItem> : MDataTable<TItem>
+{
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        HideDefaultFooter = true;
+    }
+
+    protected override void SetComponentClass()
+    {
+        base.SetComponentClass();
+
+        CssProvider.Merge(delegate (CssBuilder cssBuilder) {
+            cssBuilder.Add("default")
+                      .Add("table-border-none");
+        });
+    }
+}
+
