49753104ec9caf2e78598bdcb62921fb36d589ee
wuweilai <824255785@qq.com>
2022-05-26 14:03:33 +0800

    optimize code

--- src/Contracts/Masa.Auth.Contracts.Admin/Subjects/SyncStaffDto.cs
+++ src/Contracts/Masa.Auth.Contracts.Admin/Subjects/SyncStaffDto.cs
@@ -20,13 +20,13 @@ public class SyncStaffDto
     [ImporterHeader(Name = "工号")]
     public string JobNumber { get; set; } = "";
 
-    [ImporterHeader(Name = "手机号")]
+    [ImporterHeader(Name = "手机号码")]
     public string? PhoneNumber { get; set; }
 
     [ImporterHeader(Name = "邮箱")]
     public string? Email { get; set; }
 
-    [ImporterHeader(Name = "身份证号")]
+    [ImporterHeader(Name = "身份证号码")]
     public string? IdCard { get; set; }
 
     [ImporterHeader(Name = "昵称")]
@@ -36,19 +36,30 @@ public class SyncStaffDto
     public string? Position { get; set; }
 
     [ImporterHeader(IsIgnore = true)]
-    public GenderTypes GenderType => Enum.Parse<GenderTypes>(GenderTypeLable);
+    public GenderTypes GenderType
+    {
+        get
+        {
+            if (GenderTypeLable is "男" or "Male") return GenderTypes.Male;
+            else return GenderTypes.Female;
+        }
+    }
 
     [ImporterHeader(Name = "性别")]
     public string GenderTypeLable { get; set; } = GenderTypes.Male.ToString();
 
     [ImporterHeader(IsIgnore = true)]
-    public StaffTypes StaffType => Enum.Parse<StaffTypes>(StaffTypeLable);
+    public StaffTypes StaffType
+    {
+        get
+        {
+            if (StaffTypeLable is "内部员工" or "InternalStaff") return StaffTypes.InternalStaff;
+            else return StaffTypes.ExternalStaff;
+        }
+    }
 
     [ImporterHeader(Name = "员工类型")]
     public string StaffTypeLable { get; set; } = StaffTypes.InternalStaff.ToString();
-
-    //[ImporterHeader(Name = "备注")]
-    //public string? Remark { get; set; }
 }
 
 
