21fc652e30e9d6217d4f0f9608c64730de024dfc
Mayue <mayue@lonsid.cn>
2022-06-23 17:07:29 +0800

    feat:update permission ctor

---
+++ src/Services/Masa.Auth.Service.Admin/Infrastructure/AuthDbContextSeed.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Auth.Service.Admin.Infrastructure;
+
+public class AuthDbContextSeed
+{
+    public async Task SeedAsync(AuthDbContext context, ILogger<AuthDbContextSeed> logger)
+    {
+        var menus = new List<Permission>() {
+            new Permission("MASA_Auth","Masa_Auth_Web","User","user","User","mdi-account-outline",PermissionTypes.Menu),
+            new Permission("MASA_Auth","Masa_Auth_Web","RolePermission","RolePermission","","mdi-shield-half-full",PermissionTypes.Menu)
+        };
+    }
+}
