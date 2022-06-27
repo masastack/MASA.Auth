2e6e8ea4a12c62c2dda1a702477d66213e04002b
Mayue <mayue@lonsid.cn>
2022-06-27 10:02:50 +0800

    feat:AddSameSiteCookiePolicy

--- src/Services/Masa.Auth.Service.Admin/Services/OssService.cs
+++ src/Services/Masa.Auth.Service.Admin/Services/OssService.cs
@@ -1,40 +1,40 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-namespace Masa.Auth.Service.Admin.Services
+namespace Masa.Auth.Service.Admin.Services;
+
+public class OssService : RestServiceBase
 {
-    public class OssService : RestServiceBase
+    public OssService(IServiceCollection services) : base(services, "api/oss")
     {
-        public OssService(IServiceCollection services) : base(services, "api/oss")
-        {
-        }
+        MapGet(GetDefaultImages);
+    }
 
-        private async Task<GetSecurityTokenDto> GetSecurityTokenAsync([FromServices] IClient client, [FromServices] DaprClient daprClient)
-        {
-            var region = "oss-cn-hangzhou";
-            var response = client.GetSecurityToken();
-            var stsToken = response.SessionToken;
-            var accessId = response.AccessKeyId;
-            var accessSecret = response.AccessKeySecret;
-            var bucket = daprClient.GetSecretAsync("localsecretstore", "bucket").Result.First().Value;
-            return await Task.FromResult(new GetSecurityTokenDto(region, accessId, accessSecret, stsToken, bucket));
-        }
+    private async Task<GetSecurityTokenDto> GetSecurityTokenAsync([FromServices] IClient client, [FromServices] DaprClient daprClient)
+    {
+        var region = "oss-cn-hangzhou";
+        var response = client.GetSecurityToken();
+        var stsToken = response.SessionToken;
+        var accessId = response.AccessKeyId;
+        var accessSecret = response.AccessKeySecret;
+        var bucket = daprClient.GetSecretAsync("localsecretstore", "bucket").Result.First().Value;
+        return await Task.FromResult(new GetSecurityTokenDto(region, accessId, accessSecret, stsToken, bucket));
+    }
 
-        private List<GetDefaultImagesDto> GetDefaultImages()
+    private List<GetDefaultImagesDto> GetDefaultImages()
+    {
+        return new List<GetDefaultImagesDto>()
         {
-            return new List<GetDefaultImagesDto>()
-            {
-                new GetDefaultImagesDto(GenderTypes.Male,"https://cdn.masastack.com/stack/images/avatar/mr.chen.svg"),
-                new GetDefaultImagesDto(GenderTypes.Male,"https://cdn.masastack.com/stack/images/avatar/mr.gu.svg"),
-                new GetDefaultImagesDto(GenderTypes.Male,"https://cdn.masastack.com/stack/images/avatar/mr.ma.svg"),
-                new GetDefaultImagesDto(GenderTypes.Male,"https://cdn.masastack.com/stack/images/avatar/mr.wu.svg"),
-                new GetDefaultImagesDto(GenderTypes.Male,"https://cdn.masastack.com/stack/images/avatar/mr.xie.svg"),
-                new GetDefaultImagesDto(GenderTypes.Male,"https://cdn.masastack.com/stack/images/avatar/mr.yan.svg"),
-                new GetDefaultImagesDto(GenderTypes.Male,"https://cdn.masastack.com/stack/images/avatar/mr.zhen.svg"),
-                new GetDefaultImagesDto(GenderTypes.Male,"https://cdn.masastack.com/stack/images/avatar/mr.zhu.svg"),
-                new GetDefaultImagesDto(GenderTypes.Female,"https://cdn.masastack.com/stack/images/avatar/ms.qu.svg"),
-                new GetDefaultImagesDto(GenderTypes.Female,"https://cdn.masastack.com/stack/images/avatar/ms.wu.svg"),
-            };
-        }
+            new GetDefaultImagesDto(GenderTypes.Male,"https://cdn.masastack.com/stack/images/avatar/mr.chen.svg"),
+            new GetDefaultImagesDto(GenderTypes.Male,"https://cdn.masastack.com/stack/images/avatar/mr.gu.svg"),
+            new GetDefaultImagesDto(GenderTypes.Male,"https://cdn.masastack.com/stack/images/avatar/mr.ma.svg"),
+            new GetDefaultImagesDto(GenderTypes.Male,"https://cdn.masastack.com/stack/images/avatar/mr.wu.svg"),
+            new GetDefaultImagesDto(GenderTypes.Male,"https://cdn.masastack.com/stack/images/avatar/mr.xie.svg"),
+            new GetDefaultImagesDto(GenderTypes.Male,"https://cdn.masastack.com/stack/images/avatar/mr.yan.svg"),
+            new GetDefaultImagesDto(GenderTypes.Male,"https://cdn.masastack.com/stack/images/avatar/mr.zhen.svg"),
+            new GetDefaultImagesDto(GenderTypes.Male,"https://cdn.masastack.com/stack/images/avatar/mr.zhu.svg"),
+            new GetDefaultImagesDto(GenderTypes.Female,"https://cdn.masastack.com/stack/images/avatar/ms.qu.svg"),
+            new GetDefaultImagesDto(GenderTypes.Female,"https://cdn.masastack.com/stack/images/avatar/ms.wu.svg"),
+        };
     }
 }
--- src/Web/Masa.Auth.Web.Sso/Program.cs
+++ src/Web/Masa.Auth.Web.Sso/Program.cs
@@ -1,6 +1,8 @@
 // Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
+using Masa.Auth.Web.Sso;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -24,6 +26,7 @@ builder.Services.AddMasaIdentityModel(IdentityType.MultiEnvironment);
 builder.Services.AddAuthClient(builder.Configuration.GetValue<string>("AuthClient:Url"));
 builder.Services.AddPmClient(builder.Configuration.GetValue<string>("PmClient:Url"));
 
+builder.Services.AddSameSiteCookiePolicy();
 builder.Services.AddOidcCacheStorage(builder.Configuration.GetSection("RedisConfig").Get<RedisConfigurationOptions>())
     .AddIdentityServer(options =>
     {
@@ -36,69 +39,6 @@ builder.Services.AddOidcCacheStorage(builder.Configuration.GetSection("RedisConf
     .AddResourceOwnerValidator<ResourceOwnerPasswordValidator>()
     .AddProfileService<UserProfileService>()
     .AddCustomTokenRequestValidator<CustomTokenRequestValidator>();
-
-builder.Services.Configure<CookiePolicyOptions>(options =>
-{
-    options.MinimumSameSitePolicy = SameSiteMode.Lax;
-    options.OnAppendCookie = cookieContext =>
-       CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
-    options.OnDeleteCookie = cookieContext =>
-       CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
-});
-
-static void CheckSameSite(HttpContext httpContext, CookieOptions options)
-{
-    if (options.SameSite == SameSiteMode.None)
-    {
-        var userAgent = httpContext.Request.Headers["User-Agent"].ToString();
-        if (httpContext.Request.Scheme != "https")
-        {
-            options.SameSite = SameSiteMode.Unspecified;
-        }
-        if (DisallowsSameSiteNone(userAgent))
-        {
-            options.SameSite = SameSiteMode.Lax;
-        }
-    }
-}
-
-static bool DisallowsSameSiteNone(string userAgent)
-{
-    // Cover all iOS based browsers here. This includes:
-    // - Safari on iOS 12 for iPhone, iPod Touch, iPad
-    // - WkWebview on iOS 12 for iPhone, iPod Touch, iPad
-    // - Chrome on iOS 12 for iPhone, iPod Touch, iPad
-    // All of which are broken by SameSite=None, because they use the iOS networking
-    // stack.
-    if (userAgent.Contains("CPU iPhone OS 12") ||
-        userAgent.Contains("iPad; CPU OS 12"))
-    {
-        return true;
-    }
-
-    // Cover Mac OS X based browsers that use the Mac OS networking stack. 
-    // This includes:
-    // - Safari on Mac OS X.
-    // This does not include:
-    // - Chrome on Mac OS X
-    // Because they do not use the Mac OS networking stack.
-    if (userAgent.Contains("Macintosh; Intel Mac OS X 10_14") &&
-        userAgent.Contains("Version/") && userAgent.Contains("Safari"))
-    {
-        return true;
-    }
-
-    // Cover Chrome 50-69, because some versions are broken by SameSite=None, 
-    // and none in this range require it.
-    // Note: this covers some pre-Chromium Edge versions, 
-    // but pre-Chromium Edge does not require SameSite=None.
-    if (userAgent.Contains("Chrome/5") || userAgent.Contains("Chrome/6"))
-    {
-        return true;
-    }
-
-    return false;
-}
 
 builder.Services.AddSingleton<SsoAuthenticationStateCache>();
 builder.Services.AddScoped<AuthenticationStateProvider, SsoAuthenticationStateProvider>();
@@ -116,9 +56,8 @@ else
     app.UseHsts();
 }
 
+app.UseCookiePolicy();
 app.UseIdentityServer();
-// This cookie policy fixes login issues with Chrome 80+ using HHTP
-app.UseCookiePolicy(new CookiePolicyOptions { MinimumSameSitePolicy = SameSiteMode.Lax });
 
 app.UseHttpsRedirection();
 
