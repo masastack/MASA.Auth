53a903029538d8bbfc88cccd048f53d6ed0e3a26
Mayue <mayue@lonsid.cn>
2022-07-01 09:07:58 +0800

    feat:update blazor version

--- src/Web/Masa.Auth.Web.Admin.Rcl/Global/ServiceCollectionExtensions.cs
+++ src/Web/Masa.Auth.Web.Admin.Rcl/Global/ServiceCollectionExtensions.cs
@@ -7,17 +7,13 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddGlobalForServer(this IServiceCollection services)
     {
-        services.AddMasaI18nForServer("wwwroot/i18n");
         services.AddScoped<GlobalConfig>();
-
         return services;
     }
 
     public static async Task<IServiceCollection> AddGlobalForWasmAsync(this IServiceCollection services, string baseUri)
     {
-        await services.AddMasaI18nForWasmAsync(Path.Combine(baseUri, $"i18n"));
         services.AddScoped<GlobalConfig>();
-
         return services;
     }
 }
