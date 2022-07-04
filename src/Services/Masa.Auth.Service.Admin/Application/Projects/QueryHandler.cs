0063e2161374710e39f1db14087c5601bdd57434
Mayue <mayue@lonsid.cn>
2022-07-04 17:37:05 +0800

    feat:globla using

--- src/Services/Masa.Auth.Service.Admin/Application/Projects/QueryHandler.cs
+++ src/Services/Masa.Auth.Service.Admin/Application/Projects/QueryHandler.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using Masa.BuildingBlocks.BasicAbility.Dcc;
-
 namespace Masa.Auth.Service.Admin.Application.Projects;
 
 public class QueryHandler
