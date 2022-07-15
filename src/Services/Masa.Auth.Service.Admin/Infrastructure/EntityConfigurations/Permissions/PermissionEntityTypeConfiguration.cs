ca068d5df2c2ad6c632906ab3b3aae66bf5f40c5
Mayue <mayue@lonsid.cn>
2022-07-15 09:55:15 +0800

    feat:modify permission index

--- src/Services/Masa.Auth.Service.Admin/Infrastructure/EntityConfigurations/Permissions/PermissionEntityTypeConfiguration.cs
+++ src/Services/Masa.Auth.Service.Admin/Infrastructure/EntityConfigurations/Permissions/PermissionEntityTypeConfiguration.cs
@@ -8,7 +8,7 @@ public class PermissionEntityTypeConfiguration : IEntityTypeConfiguration<Permis
     public void Configure(EntityTypeBuilder<Permission> builder)
     {
         builder.HasKey(p => p.Id);
-        builder.HasIndex(p => new { p.AppId, p.Code }).IsUnique().HasFilter("[IsDeleted] = 0");
+        builder.HasIndex(p => new { p.SystemId, p.AppId, p.Code }).IsUnique().HasFilter("[IsDeleted] = 0");
         builder.Property(p => p.Name).HasMaxLength(20).IsRequired();
         builder.Property(p => p.Code).HasMaxLength(255).IsRequired();
         builder.Property(p => p.Url).HasMaxLength(255).IsRequired();
