c2eb92055a2094b50842245a9c943332ca18675f
Mayue <mayue@lonsid.cn>
2022-07-15 11:03:20 +0800

    update:modify permission name length

--- src/Services/Masa.Auth.Service.Admin/Infrastructure/EntityConfigurations/Permissions/PermissionEntityTypeConfiguration.cs
+++ src/Services/Masa.Auth.Service.Admin/Infrastructure/EntityConfigurations/Permissions/PermissionEntityTypeConfiguration.cs
@@ -9,7 +9,7 @@ public class PermissionEntityTypeConfiguration : IEntityTypeConfiguration<Permis
     {
         builder.HasKey(p => p.Id);
         builder.HasIndex(p => new { p.SystemId, p.AppId, p.Code }).IsUnique().HasFilter("[IsDeleted] = 0");
-        builder.Property(p => p.Name).HasMaxLength(20).IsRequired();
+        builder.Property(p => p.Name).HasMaxLength(40).IsRequired();
         builder.Property(p => p.Code).HasMaxLength(255).IsRequired();
         builder.Property(p => p.Url).HasMaxLength(255).IsRequired();
         builder.Property(p => p.Description).HasMaxLength(255);
