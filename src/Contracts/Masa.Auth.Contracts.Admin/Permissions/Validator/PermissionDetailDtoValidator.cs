3c84dc09a00961f8d7e12040a3ebf04c71492405
Mayue <mayue@lonsid.cn>
2022-07-28 15:11:10 +0800

    chore:global using

--- src/Contracts/Masa.Auth.Contracts.Admin/Permissions/Validator/PermissionDetailDtoValidator.cs
+++ src/Contracts/Masa.Auth.Contracts.Admin/Permissions/Validator/PermissionDetailDtoValidator.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using Masa.Auth.Contracts.Admin.Infrastructure.Constants;
-
 namespace Masa.Auth.Contracts.Admin.Permissions.Validator;
 
 public class PermissionDetailDtoValidator<T> : AbstractValidator<T> where T : PermissionDetailDto
