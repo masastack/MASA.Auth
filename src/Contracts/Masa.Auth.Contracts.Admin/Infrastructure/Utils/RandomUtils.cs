b515be687e2bc4febc97c6a2a48efe82cc084e5f
wuweilai <824255785@qq.com>
2022-07-28 13:22:17 +0800

    feat(RandomUtils):optimize GenerateSpecifiedString

--- src/Contracts/Masa.Auth.Contracts.Admin/Infrastructure/Utils/RandomUtils.cs
+++ src/Contracts/Masa.Auth.Contracts.Admin/Infrastructure/Utils/RandomUtils.cs
@@ -6,26 +6,23 @@ namespace Masa.Auth.Contracts.Admin.Infrastructure.Utils;
 public class RandomUtils
 {
     private const string LETTERS = "ABCDEFGHIJKMLNOPQRSTUVWXYZabcdefghigklmnopqrstuvwxyz";
-
-    private const string NUMBERS = "0123456789";
+    private const string LETTERNUMBERS = "ABCDEFGHIJKMLNOPQRSTUVWXYZabcdefghigklmnopqrstuvwxyz0123456789";
 
     public static string GenerateSpecifiedString(int length, bool includeNumbers = false)
     {
         var sb = new StringBuilder(length);
-        var numberModel = includeNumbers;
         for (var i = 0; i < length; i++)
         {          
-            if (numberModel)
+            if (includeNumbers)
             {
-                var index = Random.Shared.Next(NUMBERS.Length);
-                sb.Append(NUMBERS[index]);
+                var index = Random.Shared.Next(LETTERNUMBERS.Length);
+                sb.Append(LETTERNUMBERS[index]);
             }
             else
             {
                 var index = Random.Shared.Next(LETTERS.Length);
                 sb.Append(LETTERS[index]);
             }
-            if (includeNumbers) numberModel = !numberModel;
         }
         return sb.ToString();
     }
