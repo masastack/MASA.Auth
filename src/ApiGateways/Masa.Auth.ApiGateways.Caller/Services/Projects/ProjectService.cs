8d6182830936d7039dd0b7af76a8039602aba957
Mayue <mayue@lonsid.cn>
2022-08-09 11:22:24 +0800

    feat:global using

--- src/ApiGateways/Masa.Auth.ApiGateways.Caller/Services/Projects/ProjectService.cs
+++ src/ApiGateways/Masa.Auth.ApiGateways.Caller/Services/Projects/ProjectService.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using Masa.BuildingBlocks.Service.Caller;
-
 namespace Masa.Auth.ApiGateways.Caller.Services.Projects;
 
 public class ProjectService : ServiceBase
