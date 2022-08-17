da299add7ab318f02690d7dabcb64dbb71114b2d
Mayue <mayue@lonsid.cn>
2022-08-17 11:12:59 +0800

    add:ConsentResponseStore

---
+++ src/Web/Masa.Auth.Web.Sso/Infrastructure/Stores/ConsentResponseStore.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Auth.Web.Sso.Infrastructure.Stores;
+
+public class ConsentResponseStore : IMessageStore<ConsentResponse>
+{
+    public Task<Message<ConsentResponse>> ReadAsync(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> WriteAsync(Message<ConsentResponse> message)
+    {
+        throw new NotImplementedException();
+    }
+}
