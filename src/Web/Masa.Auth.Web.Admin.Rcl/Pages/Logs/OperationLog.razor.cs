a8b21922bb88e96320c3e3a028f976121c945fe7
wuweilai <824255785@qq.com>
2022-08-23 10:53:22 +0800

    refactor:refactpr code

--- src/Web/Masa.Auth.Web.Admin.Rcl/Pages/Logs/OperationLog.razor.cs
+++ src/Web/Masa.Auth.Web.Admin.Rcl/Pages/Logs/OperationLog.razor.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using Masa.Auth.ApiGateways.Caller.Services.Logs;
 using JsInitVariables = Masa.Stack.Components.JsInitVariables;
 
 namespace Masa.Auth.Web.Admin.Rcl.Pages.Logs;
