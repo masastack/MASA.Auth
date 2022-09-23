b28f938e4bb2093c28cb48914dd5b1cbf038ac32
wuweilai <824255785@qq.com>
2022-09-23 16:18:59 +0800

    feat(packge):update nuget packge

--- src/Services/Masa.Auth.Service.Admin/Services/MessageService.cs
+++ src/Services/Masa.Auth.Service.Admin/Services/MessageService.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using Masa.Contrib.Service.MinimalAPIs;
-
 namespace Masa.Auth.Service.Admin.Services;
 
 public class MessageService : ServiceBase
@@ -22,10 +20,5 @@ public class MessageService : ServiceBase
         [FromBody] SendEmailModel sendEmailModel)
     {
         await eventBus.PublishAsync(new SendEmailCommand(sendEmailModel));
-    }
-
-    protected override string GetMethodName(MethodInfo methodInfo, string methodName, ServiceRouteOptions globalOptions)
-    {
-        return Regex.Replace(methodName.TrimEnd("Async"), @"(\B[A-Z])", "_$1").ToLower();
     }
 }
