ad9074455a70fc2531c96a850af09c9f93cf8343
wuweilai <30889371+15168440402@users.noreply.github.com>
2022-10-12 11:18:37 +0800

    Feature(sso login):add login by local phoneNumber (#393)
    * feat(login):LocalLoginByPhoneNumberAgent
    
    * feat(login):LocalLoginByPhoneNumber
    
    * refactor code

---
+++ src/Web/Masa.Auth.Web.Sso/Controllers/AliyunController.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Auth.Web.Sso.Controllers;
+
+[AllowAnonymous]
+public class AliyunController : Controller
+{
+    readonly LocalLoginByPhoneNumberAgent _localLoginByPhoneNumberAgent;
+
+    public AliyunController(LocalLoginByPhoneNumberAgent localLoginByPhoneNumberAgent)
+    {
+        _localLoginByPhoneNumberAgent = localLoginByPhoneNumberAgent;
+    }
+
+    [HttpGet("aliyun/authToken")]
+    public Task<GetAuthTokenResponseBodyTokenInfo?> GetAuthTokenAsync()
+    {
+        return _localLoginByPhoneNumberAgent.GetAuthTokenAsync();
+    }
+}
---
+++ src/Web/Masa.Auth.Web.Sso/Infrastructure/Aliyun/LocalLoginByPhoneNumberAgent.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+using AliyunClient = AlibabaCloud.SDK.Dypnsapi20170525.Client;
+using AliyunConfig = AlibabaCloud.OpenApiClient.Models.Config;
+
+namespace Masa.Auth.Web.Sso.Infrastructure.Aliyun;
+
+public class LocalLoginByPhoneNumberAgent : IScopedDependency
+{
+    AliyunPhoneNumberLoginOptions _options;
+
+    public LocalLoginByPhoneNumberAgent(IOptions<AliyunPhoneNumberLoginOptions> options)
+    {
+        _options = options.Value;
+    }
+
+    public async Task<GetAuthTokenResponseBodyTokenInfo?> GetAuthTokenAsync(GetAuthTokenRequest? request = null)
+    {
+        var client = CreateClient();
+        request ??= new GetAuthTokenRequest
+        {
+            Url = "https://0.0.0.0",
+            Origin = "https://0.0.0.0",
+        };
+        var describePhoneNumberResaleResp = await client.GetAuthTokenAsync(request);
+        string code = describePhoneNumberResaleResp.Body.Code;
+        if (code == "OK")
+        {
+            return describePhoneNumberResaleResp.Body.TokenInfo;
+        }
+        return null;
+    }
+
+    public async Task<(bool success,string errorMsg)> VerifyPhoneWithTokenAsync(string phoneNumber, string spToken)
+    {
+        var client = CreateClient();
+        var verifyPhoneWithTokenRequest = new VerifyPhoneWithTokenRequest
+        {
+            PhoneNumber = phoneNumber,
+            SpToken = spToken,
+        };
+        var runtime = new RuntimeOptions();
+        try
+        {
+            var verifyPhoneWithTokenResponse = await client.VerifyPhoneWithTokenWithOptionsAsync(verifyPhoneWithTokenRequest, runtime);
+            if (verifyPhoneWithTokenResponse.Body.Code == "OK")
+            {
+                var verifyResult = verifyPhoneWithTokenResponse.Body.GateVerify.VerifyResult;
+                switch (verifyResult)
+                {
+                    case "PASS":
+                        return (true, "");
+                    case "REJECT":
+                        return (false, "号码验证不一致，请通过短信方式登录");
+                    case "UNKNOWN":
+                        return (false, "无法判断号码，请通过短信方式登录");
+                    default:
+                        return (false, "未知异常");
+                };
+            }
+        }
+        catch
+        {
+        }
+        return (false, "运营商接口异常");
+    }
+
+    private AliyunClient CreateClient()
+    {
+        var config = new AliyunConfig
+        {
+            AccessKeyId = _options.AccessKeyId,
+            AccessKeySecret = _options.AccessKeySecret,
+            Endpoint = _options.Endpoint,
+        };
+        return new AliyunClient(config);
+    }
+}
