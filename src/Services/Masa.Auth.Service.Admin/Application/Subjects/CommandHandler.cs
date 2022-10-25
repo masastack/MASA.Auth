1f65d208a40f82443db031fca1206a3a567e1cdf
wuweilai <824255785@qq.com>
2022-10-25 17:35:19 +0800

    feat(user bind):user bind msg code

--- src/Services/Masa.Auth.Service.Admin/Application/Subjects/CommandHandler.cs
+++ src/Services/Masa.Auth.Service.Admin/Application/Subjects/CommandHandler.cs
@@ -898,7 +898,7 @@ public class CommandHandler
     public async Task RegisterThirdPartyUserAsync(RegisterThirdPartyUserCommand command)
     {
         var model = command.Model;
-        await RegisterVerifyAsync(model);
+        await BindVerifyAsync(model);
         var addThirdPartyUserExternalCommand = new AddThirdPartyUserExternalCommand(new AddThirdPartyUserModel
         {
             ThridPartyIdentity = model.ThridPartyIdentity,
@@ -916,6 +916,25 @@ public class CommandHandler
         }, true);
         await _eventBus.PublishAsync(addThirdPartyUserExternalCommand);
         command.Result = addThirdPartyUserExternalCommand.Result;
+    }
+
+    async Task BindVerifyAsync(RegisterByEmailModel model)
+    {
+        if (model.UserRegisterType == UserRegisterTypes.Email)
+        {
+            var emailCodeKey = CacheKey.EmailCodeBindKey(model.Email);
+            var emailCode = await _cache.GetAsync<string>(emailCodeKey);
+            if (!model.EmailCode.Equals(emailCode))
+            {
+                throw new UserFriendlyException("Invalid Email verification code");
+            }
+        }
+        var smsCodeKey = CacheKey.MsgCodeForBindKey(model.PhoneNumber);
+        var smsCode = await _cache.GetAsync<string>(smsCodeKey);
+        if (!model.SmsCode.Equals(smsCode))
+        {
+            throw new UserFriendlyException("Invalid SMS verification code");
+        }
     }
 
     [EventHandler(1)]
