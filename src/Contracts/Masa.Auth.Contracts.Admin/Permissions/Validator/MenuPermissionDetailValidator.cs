2de8012f240d735c805a45b2c01434b099081377
MayueCif <6133287+MayueCif@users.noreply.github.com>
2022-11-29 09:52:37 +0800

    feat:permission validator (#569)
    Co-authored-by: Mayue <mayue@lonsid.cn>

---
+++ src/Contracts/Masa.Auth.Contracts.Admin/Permissions/Validator/MenuPermissionDetailValidator.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Auth.Contracts.Admin.Permissions.Validator;
+
+public class MenuPermissionDetailValidator : AbstractValidator<MenuPermissionDetailDto>
+{
+    public MenuPermissionDetailValidator()
+    {
+        Include(new PermissionDetailValidator<MenuPermissionDetailDto>());
+    }
+}
--- src/Contracts/Masa.Auth.Contracts.Admin/Permissions/Validator/PermissionDetailValidator.cs
+++ src/Contracts/Masa.Auth.Contracts.Admin/Permissions/Validator/PermissionDetailValidator.cs
@@ -3,17 +3,17 @@
 
 namespace Masa.Auth.Contracts.Admin.Permissions.Validator;
 
-public class PermissionDetailDtoValidator<T> : AbstractValidator<T> where T : PermissionDetailDto
+public class PermissionDetailValidator<T> : AbstractValidator<T> where T : PermissionDetailDto
 {
-    public PermissionDetailDtoValidator()
+    public PermissionDetailValidator()
     {
         RuleFor(p => p).NotNull().WithMessage($"Parameter error");
         RuleFor(p => p.Name).ChineseLetterNumber().Must(name => !string.IsNullOrWhiteSpace(name) && name.Length <= 20)
             .WithMessage("Permission Name can`t null and length must be less than 20.");
         RuleFor(p => p.Description).ChineseLetterNumber().Must(description => description.Length <= 255)
             .WithMessage("Permission Description length must be less than 255.");
-        RuleFor(p => p.AppId).NotNull().WithMessage("AppId can`t null");
-        RuleFor(p => p.Code).NotNull().WithMessage("Code can`t null");
+        RuleFor(p => p.AppId).Must(appId => !string.IsNullOrEmpty(appId.Trim())).WithMessage("AppId can`t empty");
+        RuleFor(p => p.Code).Must(code => !string.IsNullOrEmpty(code.Trim())).WithMessage("Code can`t empty");
         RuleFor(p => p.Order).NotNull().Must(order => order >= BusinessConsts.PERMISSION_ORDER_MIN_VALUE
             && order <= BusinessConsts.PERMISSION_ORDER_MAX_VALUE)
             .WithMessage($"The valid value of order is between {BusinessConsts.PERMISSION_ORDER_MIN_VALUE}-{BusinessConsts.PERMISSION_ORDER_MAX_VALUE}");
