bcc57c86e70760a1a61f307e4b6e97d10bf4c831
Mayue <mayue8373@live.com>
2023-03-03 20:06:25 +0800

    feat:update using

--- src/Services/Masa.Auth.Service.Admin/Program.cs
+++ src/Services/Masa.Auth.Service.Admin/Program.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using Masa.Contrib.StackSdks.Middleware;
-
 var builder = WebApplication.CreateBuilder(args);
 
 ValidatorOptions.Global.LanguageManager = new MasaLanguageManager();
