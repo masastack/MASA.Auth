be619f0d41c4f35258dea18eb43b4cd157cb3f3f
wuweilai <824255785@qq.com>
2023-03-07 13:16:13 +0800

    fix sync staff prompt information is missing

--- src/Web/Masa.Auth.Web.Admin.Rcl/Shared/AdminCompontentBase.cs
+++ src/Web/Masa.Auth.Web.Admin.Rcl/Shared/AdminCompontentBase.cs
@@ -82,8 +82,11 @@ public abstract class AdminCompontentBase : ComponentBase
     public string T(string key)
     {
         if (string.IsNullOrEmpty(key)) return key;
-        if (PageName is not null) return I18n.T(PageName, key, false) ?? I18n.T(key, false)!;
-        else return I18n.T(key, true)!;
+        if (PageName is not null)
+        {
+            return I18n.T(PageName, key, false) ?? I18n.T(key, false, false) ?? key;
+        }
+        else return I18n.T(key, true, false) ?? key;
     }
 
     public string T(string formatkey, params string[] args)
