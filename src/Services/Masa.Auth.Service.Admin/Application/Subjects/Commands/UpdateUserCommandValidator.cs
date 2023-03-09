6f4fab4d8b1797100f320c3ed1a398ca2f1d2b21
wuweilai <30889371+wuweilaiya@users.noreply.github.com>
2023-03-09 16:56:57 +0800

    Fix issues 0309 (#764)
    * fix upsert user validate
    
    * fix sync staff
    
    * refactor code

--- src/Services/Masa.Auth.Service.Admin/Application/Subjects/Commands/UpdateUserCommandValidator.cs
+++ src/Services/Masa.Auth.Service.Admin/Application/Subjects/Commands/UpdateUserCommandValidator.cs
@@ -3,16 +3,16 @@
 
 namespace Masa.Auth.Service.Admin.Application.Subjects.Commands;
 
-public class UpdateUserCommandValidator : AbstractValidator<UpdateUserCommand>
+public class UpdateUserCommandValidator : MasaAbstractValidator<UpdateUserCommand>
 {
     public UpdateUserCommandValidator()
     {
         RuleFor(command => command.User.DisplayName).Required().MaximumLength(50);
-        RuleFor(command => command.User.Name).ChineseLetterNumber().MaximumLength(20);
+        WhenNotNullOrEmpty(command => command.User.Name, roleBuilder => roleBuilder.ChineseLetterNumber().MaximumLength(20));
         RuleFor(command => command.User.PhoneNumber).Phone();
-        RuleFor(command => command.User.Email).Email();
-        When(command => !string.IsNullOrEmpty(command.User.IdCard), () => RuleFor(command => command.User.IdCard).IdCard());
-        When(command => !string.IsNullOrEmpty(command.User.CompanyName), () => RuleFor(command => command.User.CompanyName).ChineseLetterNumber().MaximumLength(50));
-        When(command => !string.IsNullOrEmpty(command.User.Position), () => RuleFor(command => command.User.Position).ChineseLetterNumber().MaximumLength(20));
+        WhenNotNullOrEmpty(command => command.User.Email, roleBuilder => roleBuilder.Email());
+        WhenNotNullOrEmpty(command => command.User.IdCard, roleBuilder => roleBuilder.IdCard());
+        WhenNotNullOrEmpty(command => command.User.CompanyName, roleBuilder => roleBuilder.ChineseLetterNumber().MaximumLength(50));
+        WhenNotNullOrEmpty(command => command.User.Position, roleBuilder => roleBuilder.ChineseLetterNumber().MaximumLength(20));
     }
 }
