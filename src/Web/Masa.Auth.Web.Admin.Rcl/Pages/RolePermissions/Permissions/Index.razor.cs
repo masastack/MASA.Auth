b646d65413bbd478b611a32b41f5a90b313f35cf
Kogel <935368322@qq.com>
2023-03-14 15:10:26 +0800

    feat:The optimization menu cannot be at the same level as the element

--- src/Web/Masa.Auth.Web.Admin.Rcl/Pages/RolePermissions/Permissions/Index.razor.cs
+++ src/Web/Masa.Auth.Web.Admin.Rcl/Pages/RolePermissions/Permissions/Index.razor.cs
@@ -83,10 +83,6 @@ public partial class Index
                         RemoveChildElementAll(menu.Children);
                     }
                 }
-                else
-                {
-                    continue;
-                }
             }
         }
     }
