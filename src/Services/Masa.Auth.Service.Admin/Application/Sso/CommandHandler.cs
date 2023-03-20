9cfd5f2b35ec0d1f3a04533c005e44b8de62180b
Li Ming <1028139084@qq.com>
2023-03-20 13:48:38 +0800

    i18 namespace global using

--- src/Services/Masa.Auth.Service.Admin/Application/Sso/CommandHandler.cs
+++ src/Services/Masa.Auth.Service.Admin/Application/Sso/CommandHandler.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using Masa.BuildingBlocks.Globalization.I18n;
-
 namespace Masa.Auth.Service.Admin.Application.Sso;
 
 public class CommandHandler
