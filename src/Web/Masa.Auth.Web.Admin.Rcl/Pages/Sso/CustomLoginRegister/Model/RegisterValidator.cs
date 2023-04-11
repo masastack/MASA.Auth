77202cac034f1e000e50ee0b39ffc79fe773080a
yazhouzhang <y_az@163.com>
2023-04-11 17:37:46 +0800

    Fix: Add or Edit custom login: Form validator, I18n Name changed to T… (#955)
    * Fix: Add or Edit custom login: Form validator, I18n Name changed to TitleName, Prevents internal Tab switching, Add scrollbar
    
    * move to global using, Fix I18n

--- src/Web/Masa.Auth.Web.Admin.Rcl/Pages/Sso/CustomLoginRegister/Model/RegisterValidator.cs
+++ src/Web/Masa.Auth.Web.Admin.Rcl/Pages/Sso/CustomLoginRegister/Model/RegisterValidator.cs
@@ -3,41 +3,41 @@
 
 namespace Masa.Auth.Web.Admin.Rcl.Pages.Sso.CustomLoginRegister.Model;
 
-public class RegisterValidator : AbstractValidator<RegisterModel>
+public class RegisterValidator : MasaAbstractValidator<RegisterModel>
 {
     public RegisterValidator()
     {
-        RuleFor(register => register.Account).Required().ChineseLetterNumber();
-
-        RuleFor(register => register.Password).Required().AuthPassword();
-
-        RuleFor(register => register.ConfirmPassword)
-          .RequiredIf(register => register.CheckRequired(nameof(RegisterModel.ConfirmPassword)))
-          .Must((register, value) => register.ConfirmPassword == register.Password)
-          .WithMessage("The password is inconsistent with the confirm password")
-          .Matches(BusinessConsts.PASSWORD_REGULAR)
-          .WithMessage("Password must contain numbers and letter, and not less than 6 digits")
-          .MaximumLength(30);
-
-        RuleFor(register => register.DisplayName)
-            .RequiredIf(register => register.CheckRequired(nameof(RegisterModel.DisplayName)))
-            .ChineseLetter().MaximumLength(20);
-
-        RuleFor(register => register.Name)
-            .RequiredIf(register => register.CheckRequired(nameof(RegisterModel.Name)))
-            .ChineseLetter().MaximumLength(20);
-
         RuleFor(register => register.PhoneNumber)
-            .RequiredIf(register => register.CheckRequired(nameof(RegisterModel.PhoneNumber)))
-            .Phone();
+            .RequiredIf(register => register.CheckRequired(nameof(RegisterModel.PhoneNumber))).Phone();
 
+        _ = WhenNotEmpty(r => r.Account, rule => rule.ChineseLetterNumber());
+        RuleFor(register => register.Account).RequiredIf(register => register.CheckRequired(nameof(RegisterModel.Account)));
+
+        _ = WhenNotEmpty(r => r.Password, rule => rule.AuthPassword());
+        RuleFor(register => register.Password).RequiredIf(register => register.CheckRequired(nameof(RegisterModel.Password)));
+
+        _ = WhenNotEmpty(r => r.ConfirmPassword, rule => rule.Must((register, value) => 
+            register.ConfirmPassword == register.Password).WithMessage("The password is inconsistent with the confirm password")
+            .Matches(BusinessConsts.PASSWORD_REGULAR).WithMessage("Password must contain numbers and letter, and not less than 6 digits")
+            .MaximumLength(30));
+        RuleFor(register => register.ConfirmPassword)
+          .RequiredIf(register => register.CheckRequired(nameof(RegisterModel.Password)));
+
+        _ = WhenNotEmpty(r => r.DisplayName, rule => rule.ChineseLetter().MaximumLength(20));
+        RuleFor(register => register.DisplayName)
+            .RequiredIf(register => register.CheckRequired(nameof(RegisterModel.DisplayName)));
+
+        _ = WhenNotEmpty(r => r.Name, rule => rule.ChineseLetter().MaximumLength(20));
+        RuleFor(register => register.Name)
+            .RequiredIf(register => register.CheckRequired(nameof(RegisterModel.Name)));
+
+        _ = WhenNotEmpty(r => r.Email, rule => rule.Email());
         RuleFor(register => register.Email)
-            .RequiredIf(register => register.CheckRequired(nameof(RegisterModel.Email)))
-            .Email();
+            .RequiredIf(register => register.CheckRequired(nameof(RegisterModel.Email)));
 
+        _ = WhenNotEmpty(r => r.IdCard, rule => rule.IdCard());
         RuleFor(register => register.IdCard)
-            .RequiredIf(register => register.CheckRequired(nameof(RegisterModel.IdCard)))
-            .IdCard();
+            .RequiredIf(register => register.CheckRequired(nameof(RegisterModel.IdCard)));
     }
 }
 
