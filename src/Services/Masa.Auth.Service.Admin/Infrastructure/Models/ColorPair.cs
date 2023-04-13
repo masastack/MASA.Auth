f36524a9d6c49789a78c774c19644bc200fac22d
MayueCif <6133287+MayueCif@users.noreply.github.com>
2023-04-13 19:40:36 +0800

    feat:udpate team avatar (#989)

---
+++ src/Services/Masa.Auth.Service.Admin/Infrastructure/Models/ColorPair.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Auth.Service.Admin.Infrastructure.Models;
+
+public class ColorPair
+{
+    public string FrontColor { get; set; }
+
+    public string BackColor { get; set; }
+
+    public ColorPair(string frontColor, string backColor)
+    {
+        FrontColor = frontColor;
+        BackColor = backColor;
+    }
+}
