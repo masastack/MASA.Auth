466288f4f21d1594310da69abed9813be7c5a5c5
zhendong.dong <dongzhendong@masastack.com>
2023-04-17 18:07:54 +0800

    Update src/Web/Masa.Auth.Web.Admin.Rcl/Pages/RolePermissions/Permissions/Index.razor.cs

--- src/Web/Masa.Auth.Web.Admin.Rcl/Pages/RolePermissions/Permissions/Index.razor.cs
+++ src/Web/Masa.Auth.Web.Admin.Rcl/Pages/RolePermissions/Permissions/Index.razor.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using static Nest.JoinField;
 
 namespace Masa.Auth.Web.Admin.Rcl.Pages.RolePermissions.Permissions;
 
