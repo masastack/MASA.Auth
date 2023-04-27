a5a0147069222efa172b4e7ca36b92a8059aeeee
Li Ming <1028139084@qq.com>
2023-04-27 14:54:43 +0800

    delete using

--- src/Web/Masa.Auth.Web.Admin.Rcl/Pages/Component/Permissions/PermissionExtensionConfigure.cs
+++ src/Web/Masa.Auth.Web.Admin.Rcl/Pages/Component/Permissions/PermissionExtensionConfigure.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using System.Linq;
-
 namespace Masa.Auth.Web.Admin.Rcl.Pages.Component.Permissions;
 
 public class PermissionExtensionConfigure : PermissionsConfigure
