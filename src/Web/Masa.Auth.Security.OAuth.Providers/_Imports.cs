4fe5c0ada81aca244dd886c80faec95f3037da1c
wuweilai <30889371+wuweilaiya@users.noreply.github.com>
2023-05-06 17:38:26 +0800

    Feature(login):support apple login (#1100)
    * feat:apple login
    
    * feat:support apple login
    
    * update nuget packge
    
    * refactor code

--- src/Web/Masa.Auth.Security.OAuth.Providers/_Imports.cs
+++ src/Web/Masa.Auth.Security.OAuth.Providers/_Imports.cs
@@ -1,15 +1,18 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
+global using AspNet.Security.OAuth.Apple;
 global using AspNet.Security.OAuth.GitHub;
 global using AspNet.Security.OAuth.Weixin;
 global using Masa.Auth.Security.OAuth.Providers;
+global using Masa.Auth.Security.OAuth.Providers.Infrastructure.ThirdPartyIdpCaller;
 global using Masa.Auth.Security.OAuth.Providers.Middlewares;
 global using Masa.BuildingBlocks.Caching;
 global using Masa.BuildingBlocks.StackSdks.Auth.Contracts.Enum;
 global using Microsoft.AspNetCore.Authentication;
 global using Microsoft.AspNetCore.Authentication.Cookies;
 global using Microsoft.AspNetCore.Authentication.OAuth;
+global using Microsoft.AspNetCore.Authentication.OAuth.Claims;
 global using Microsoft.AspNetCore.Http;
 global using Microsoft.AspNetCore.WebUtilities;
 global using Microsoft.Extensions.DependencyInjection;
@@ -23,4 +26,3 @@ global using System.Security.Claims;
 global using System.Text.Encodings.Web;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
-global using Masa.Auth.Security.OAuth.Providers.Infrastructure.ThirdPartyIdpCaller;
