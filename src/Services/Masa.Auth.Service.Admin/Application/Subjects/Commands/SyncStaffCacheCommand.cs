06af69267b02eabc74f5adb43254ec491502433c
MayueCif <6133287+MayueCif@users.noreply.github.com>
2023-08-31 16:26:41 +0800

    feat:remove unused command (#1236)

--- src/Services/Masa.Auth.Service.Admin/Application/Subjects/Commands/SyncStaffCacheCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) MASA Stack All rights reserved.
-// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
-
-namespace Masa.Auth.Service.Admin.Application.Subjects.Commands;
-
-public record SyncStaffCacheCommand() : Command
-{
-    public SyncStaffResultsDto Result { get; set; } = new();
-}
