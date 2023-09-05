83b4ed15a0b3b752cef54938842c061325d528ec
Mayue <mayue8373@live.com>
2023-09-05 11:38:53 +0800

    fix:password

--- src/Contracts/Masa.Auth.Contracts.Admin/Infrastructure/Password/PasswordHelper.cs
+++ src/Contracts/Masa.Auth.Contracts.Admin/Infrastructure/Password/PasswordHelper.cs
@@ -35,11 +35,6 @@ public class PasswordHelper : ISingletonDependency
             {
                 //ignore
             }
-            finally
-            {
-                passwordRule = DEFAULTPASSWORDRULE;
-            }
-
         }
         return passwordRule;
     }
