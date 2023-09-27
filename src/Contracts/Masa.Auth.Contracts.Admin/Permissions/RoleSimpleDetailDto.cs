5b08ffcacde08036611eec59d9197519e72c86f3
MayueCif <6133287+MayueCif@users.noreply.github.com>
2023-09-27 13:27:17 +0800

    feat:add role servier (#1244)

---
+++ src/Contracts/Masa.Auth.Contracts.Admin/Permissions/RoleSimpleDetailDto.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Auth.Contracts.Admin.Permissions;
+
+public class RoleSimpleDetailDto
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Code { get; set; } = string.Empty;
+
+    public bool Enabled { get; set; }
+
+    public List<RoleSimpleDetailDto> Children { get; set; } = new();
+}
