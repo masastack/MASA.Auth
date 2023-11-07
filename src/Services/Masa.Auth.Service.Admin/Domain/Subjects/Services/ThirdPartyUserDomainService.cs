1e51f813a52b1c4557ac11620bdc012b0f12d57c
MayueCif <mayue8373@live.com>
2023-11-07 12:05:06 +0800

    feat:add user creationmodificationtime (#1252)

--- src/Services/Masa.Auth.Service.Admin/Domain/Subjects/Services/ThirdPartyUserDomainService.cs
+++ src/Services/Masa.Auth.Service.Admin/Domain/Subjects/Services/ThirdPartyUserDomainService.cs
@@ -8,23 +8,31 @@ public class ThirdPartyUserDomainService : DomainService
     readonly AuthDbContext _authDbContext;
     readonly UserDomainService _userDomainService;
     readonly IThirdPartyUserRepository _thirdPartyUserRepository;
+    readonly ILogger<ThirdPartyUserDomainService> _logger;
 
     public ThirdPartyUserDomainService(
         UserDomainService userDomainService,
         AuthDbContext authDbContext,
-        IThirdPartyUserRepository thirdPartyUserRepository)
+        IThirdPartyUserRepository thirdPartyUserRepository,
+        ILogger<ThirdPartyUserDomainService> logger)
     {
         _userDomainService = userDomainService;
         _authDbContext = authDbContext;
         _thirdPartyUserRepository = thirdPartyUserRepository;
+        _logger = logger;
     }
 
     public async Task<UserModel> AddThirdPartyUserAsync(AddThirdPartyUserDto dto)
     {
         var userDto = dto.User;
+        _logger.LogWarning("AddThirdPartyUserAsync user {0}", JsonSerializer.Serialize(userDto));
         var user = new User(userDto.Name, userDto.DisplayName ?? "", userDto.Avatar, userDto.Account, userDto.Password, "", userDto.Email, userDto.PhoneNumber ?? "",
              new ThirdPartyUser(dto.ThirdPartyIdpId, dto.ThridPartyIdentity, dto.ExtendedData));
-        var (existUser, _) = await _userDomainService.VerifyRepeatAsync(userDto.PhoneNumber, userDto.Email, default, userDto.Account);
+        var (existUser, e) = await _userDomainService.VerifyRepeatAsync(userDto.PhoneNumber, userDto.Email, default, userDto.Account);
+        if (e != null)
+        {
+            _logger.LogError("User exception:{0}", e.Message);
+        }
         if (existUser != null)
         {
             var thirdPartyUser = new ThirdPartyUser(dto.ThirdPartyIdpId, existUser.Id, dto.ThridPartyIdentity, dto.ExtendedData);
