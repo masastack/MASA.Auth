61808172dd4a25aa5ae437bb3384ba032774ef4e
wzh425 <wuzihao425@outlook.com>
2024-04-02 10:02:57 +0800

    fix: Using conflict

--- src/Services/Masa.Auth.Service.Admin/Domain/Subjects/Aggregates/PasswordType.cs
+++ src/Services/Masa.Auth.Service.Admin/Domain/Subjects/Aggregates/PasswordType.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
+using Microsoft.AspNetCore.Identity;
+
 namespace Masa.Auth.Service.Admin.Domain.Subjects.Aggregates;
 
 public class PasswordType : Enumeration
