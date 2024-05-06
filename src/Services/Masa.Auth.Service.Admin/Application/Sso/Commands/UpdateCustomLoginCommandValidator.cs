222f8ece8d9e3150c91868d6ecff868d714d5cf8
wzh425 <wuzihao425@outlook.com>
2024-05-06 14:36:32 +0800

    fix: SSO Environment Lost，issues 1183 (#1290)
    * fix: Domain account synchronization backend execution and logic adjustment
    
    * close popups
    
    * fix: Generate Staff during Ldap login
    
    * fix： issues 1269、1237、1226
    
    * fix：issuers 1183、1128
    
    * fix: Environment lost

---
+++ src/Services/Masa.Auth.Service.Admin/Application/Sso/Commands/UpdateCustomLoginCommandValidator.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Auth.Service.Admin.Application.Sso.Commands;
+
+public class UpdateCustomLoginCommandValidator : AbstractValidator<UpdateCustomLoginCommand>
+{
+    public UpdateCustomLoginCommandValidator() => RuleFor(cmd => cmd.CustomLogin).SetValidator(new UpdateCustomLoginValidator());
+}
