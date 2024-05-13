1b8023d688b6dfabea0a417a94ef7f4b64524631
wzh425 <wuzihao425@outlook.com>
2024-05-13 14:42:11 +0800

    fix: The compatible user claim is outdated (#1301)
    * fix: issues 1183、1166
    
    * fix: issues 1291
    
    * fix：Client token configuration not displayed
    
    * fix：issues 1085、915
    
    * fix: GetClaimValuesAsync

---
+++ src/Services/Masa.Auth.Service.Admin/Infrastructure/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Auth.Service.Admin.Infrastructure.Extensions;
+
+public static class DictionaryExtensions
+{
+    public static void AddOrUpdate<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue value)
+    {
+        if (dictionary.ContainsKey(key))
+        {
+            dictionary[key] = value;
+        }
+        else
+        {
+            dictionary.Add(key, value);
+        }
+    }
+}
