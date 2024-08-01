8f80a92686458a52b847f0436a09b683cbe0ec17
wzh425 <wuzihao425@outlook.com>
2024-08-01 16:04:12 +0800

    🆕 feat: Add ThirdPartyUserFieldValueAsync (#1339)
    * 🆕 feat: Add ThirdPartyUserFieldValueAsync
    
    * refactor: Remove redundant code

---
+++ src/Contracts/Masa.Auth.Contracts.Admin/Subjects/GetThirdPartyUserFieldValueDto.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Auth.Contracts.Admin.Subjects;
+
+public class GetThirdPartyUserFieldValueDto
+{
+    public string Scheme { get; set; } = default!;
+
+    public string? Field { get; set; }
+
+    public List<Guid> UserIds { get; set; } = new();
+}
---
+++ src/Services/Masa.Auth.Service.Admin/Application/Subjects/Queries/ThirdPartyUserFieldValueQuery.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Auth.Service.Admin.Application.Subjects.Queries;
+
+public record ThirdPartyUserFieldValueQuery(string Scheme, List<Guid> UserIds, string? Field) : Query<Dictionary<Guid, string>>
+{
+    public override Dictionary<Guid, string> Result { get; set; } = new();
+}
