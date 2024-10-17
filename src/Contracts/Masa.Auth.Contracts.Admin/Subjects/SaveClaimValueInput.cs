266b2c801db96aa8962e4cfb9443bb277ef80672
wzh425 <wuzihao425@outlook.com>
2024-10-17 15:40:53 +0800

    feat: Add SaveClaimValueAsync

---
+++ src/Contracts/Masa.Auth.Contracts.Admin/Subjects/SaveClaimValueInput.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Auth.Contracts.Admin.Subjects;
+
+public class SaveClaimValueInput
+{
+    public Guid UserId { get; set; }
+
+    public string ClaimName { get; set; } = default!;
+
+     public string ClaimValue { get; set; } = string.Empty;
+}
