671de46ab7c16ba8ef2bbd43ccffb9b8d487a1ce
wzh425 <wuzihao425@outlook.com>
2025-04-24 15:05:07 +0800

    style: Blank line

--- src/Domain/Masa.Auth.Domain/DynamicRoles/Aggregates/DynamicRoleDataType.cs
+++ src/Domain/Masa.Auth.Domain/DynamicRoles/Aggregates/DynamicRoleDataType.cs
@@ -24,7 +24,6 @@ public class DynamicRoleDataType : Enumeration
 
     public static DynamicRoleDataType StartNew(string type) => type switch
     {
-
         nameof(UserInfo) => new UserInfoData(),
         nameof(UserClaim) => new UserClaimData(),
         _ => new DynamicRoleDataType()
