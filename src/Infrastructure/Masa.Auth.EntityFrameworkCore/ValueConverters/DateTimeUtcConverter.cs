f085ce2f2a03aebfa5f13125e92a83f9b557314b
MayueCif <mayue8373@live.com>
2025-06-13 11:20:12 +0800

    chore: clean up using directives and references
    Removed unnecessary using directives in `AuthDbContext.cs` and `DateTimeUtcConverter.cs`.
    Updated `_Imports.cs` by removing multiple global using statements and adding new ones related to `Masa.Auth.Contracts.Admin` and `Masa.Auth.EntityFrameworkCore.PostgreSql.Converters`.

--- src/Infrastructure/Masa.Auth.EntityFrameworkCore/ValueConverters/DateTimeUtcConverter.cs
+++ src/Infrastructure/Masa.Auth.EntityFrameworkCore/ValueConverters/DateTimeUtcConverter.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-
 namespace Masa.Auth.EntityFrameworkCore.PostgreSql.Converters;
 
 public class DateTimeUtcConverter : ValueConverter<DateTime, DateTime>
