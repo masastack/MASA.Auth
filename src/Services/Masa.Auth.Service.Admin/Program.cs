c822d6209c9fa7bed19050bb37494651ad9b8758
MayueCif <mayue8373@live.com>
2025-06-17 14:15:39 +0800

    feat: Update validation settings and remove unused service
    Removed the reference to `Masa.Auth.Service.Admin.Domain.DynamicRoles.Services`.
    Added `ValidatorOptions.Global.LanguageManager = new MasaLanguageManager();`
    and set the default culture for global validation to Chinese (Simplified).

--- src/Services/Masa.Auth.Service.Admin/Program.cs
+++ src/Services/Masa.Auth.Service.Admin/Program.cs
@@ -1,7 +1,5 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
-using Masa.Auth.Service.Admin.Domain.DynamicRoles.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 ValidatorOptions.Global.LanguageManager = new MasaLanguageManager();
