175df525f414d86dc8ea5918ee9e39dea0e55ae0
MayueCif <mayue8373@live.com>
2025-10-09 10:02:36 +0800

    新增用户角色查询功能
    在 `UserHasRolesQuery.cs` 中新增 `UserHasRolesQuery` 类，用于查询用户是否具有指定角色。
    在 `QueryHandler.cs` 中添加了 `UserHasRolesQuery` 的事件处理逻辑，支持查询用户直接拥有的角色和动态角色。
    在 `UserService.cs` 中新增了一个 POST 路由 `/roles`，通过 API 提供用户角色查询功能。

---
+++ src/Services/Masa.Auth.Service.Admin/Application/Subjects/Queries/UserHasRolesQuery.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Auth.Service.Admin.Application.Subjects.Queries;
+
+public record UserHasRolesQuery(Guid UserId, List<Guid> RoleIds) : Query<List<Guid>>
+{
+    public override List<Guid> Result { get; set; } = new();
+}
