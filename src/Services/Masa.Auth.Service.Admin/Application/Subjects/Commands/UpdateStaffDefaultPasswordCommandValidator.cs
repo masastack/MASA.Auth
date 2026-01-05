7d56840d6cc03db85d791ed5462b0888cf3a9d0b
MayueCif <mayue8373@live.com>
2026-01-05 17:10:10 +0800

    Fix/staff default pwd empty (#1457)

---
+++ src/Services/Masa.Auth.Service.Admin/Application/Subjects/Commands/UpdateStaffDefaultPasswordCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Auth.Service.Admin.Application.Subjects.Commands;
+
+public class UpdateStaffDefaultPasswordCommandValidator : AbstractValidator<UpdateStaffDefaultPasswordCommand>
+{
+    public UpdateStaffDefaultPasswordCommandValidator()
+    {
+        RuleFor(command => command.DefaultPassword).SetValidator(new UpdateStaffDefaultPasswordValidator());
+    }
+}
